6b04cdc68dd890e9c9e35f5693bc0f8299893e22
Sunil Chauhan <sunil121ml@gmail.com>
2022-08-25 16:18:37 +0530

    configuring firebase app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./app.css";
+import "./App.css";
 
 function App() {
   return <div className="App">App</div>;
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCk4MQYABDbCfr1X3WslnyGkAu1Lbo7dfA",
+  authDomain: "restaurant-app-9baa0.firebaseapp.com",
+  databaseURL:
+    "https://restaurant-app-9baa0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "restaurant-app-9baa0",
+  storageBucket: "restaurant-app-9baa0.appspot.com",
+  messagingSenderId: "807288052468",
+  appId: "1:807288052468:web:47fdcf6ecb29bfb3155ca1",
+};
+
+const app = initializeApp(firebaseConfig);
