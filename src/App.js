918e8613ada5105a0d1fe62dc0e1432807fb2999
Sunil Chauhan <sunil121ml@gmail.com>
2022-08-28 17:01:42 +0530

    adding navbar in header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-blue-600">
-      App
+    <div className="w-screen h-auto flex flex-col bg-primary">
+      <Header />
+      <main className=" mt-24 p-8 w-full">Main container</main>
     </div>
   );
 }
